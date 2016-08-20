3f9645e694818dc0e9df19f7f0f329ec4d7fee90
Alexander <arfrank3@gmail.com>
2016-08-19 23:18:35 -0600

    restyled navbar and home page

---
+++ trips/static/navbar.js
@@ -0,0 +1,24 @@
+// jQuery to collapse the navbar on scroll
+var regex = new RegExp("/trips/.*/");
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50 || (regex.test(top.location.pathname)))
+    {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
