a9567f4e5f5fae91aa040130680efb67e5c1f34a
Alexander <arfrank3@gmail.com>
2016-08-20 22:42:06 -0600

    switched back to debug

--- tripshare/settings.py
+++ tripshare/settings.py
@@ -39,9 +39,9 @@ STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [".skander-ideation.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
